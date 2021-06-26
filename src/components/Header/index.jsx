d9cf722bdbd380b40b67cb09c3d3c31d96c7d2f5
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-26 17:17:03 +0800

    ✨ Add Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import styles from "src/components/Header/Header.module.css"
+
+export const Header = () => {
+  return(
+    <header className={styles.header}>
+      <Link href="/"><a className={styles.anchor}>Index</a></Link>
+      <Link href="/about"><a className={styles.anchor}>About</a></Link>
+    </header>
+  )
+}
