f84ea9704b64070b0fb29a4f64f4c740b4e45e68
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-27 17:16:59 +0800

    ✨ Add handleChange (string and boolean of useState)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,23 +3,37 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect , useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
-  const [count, setCount] = useState(1)
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
-    if(count < 10){
+    if (count < 10) {
       setCount((count) => count + 1);
     }
-  },[count])
+  }, [count]);
 
-  useEffect(()=>{
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください！");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  },[]);
+
+  useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
-    }
-  },[])
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -27,8 +41,10 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
