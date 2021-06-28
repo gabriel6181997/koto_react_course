6d17e3e048cb14a2d7d342a6ea4d0c928ebbd779
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-28 18:16:33 +0800

    🎨 Add space

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -1,7 +1,6 @@
 import styles from "src/components/Headline/Headline.module.css";
 
 export const Headline = (props) => {
-
   return (
     <div>
       <h1 className={styles.title}>{props.page} Page</h1>
@@ -9,8 +8,6 @@ export const Headline = (props) => {
         アイテムの数は {props.children}個です
       </p>
       <button onClick={props.handleReduce}>減らす</button>
-
-
     </div>
   );
 };
