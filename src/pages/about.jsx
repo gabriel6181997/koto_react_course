a58457a38205c2dcaa3418db349619220d7775fb
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-28 18:17:19 +0800

    ✨ Use useMemo

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,7 +6,7 @@ import { Header } from "src/components/Header";
 
 export default function About(props) {
   const {
-    count,
+    doubleCount,
     isShow,
     handleClick,
     handleDisplay,
@@ -23,7 +23,7 @@ export default function About(props) {
       </Head>
       <Header />
 
-      {isShow ? <h1>{count}</h1> : null}
+      {isShow ? <h1>{doubleCount}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
 
