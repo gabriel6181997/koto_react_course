7ff4ceedb0a6b508209f4abba96ac54877099755
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-30 20:59:06 +0800

    ✨ fetch users' data and create fetcher.js

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -1,18 +1,18 @@
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
-import { useRouter } from "next/router";
+import { Post } from "src/components/Post";
+import { usePost } from "src/hooks/usePost";
 
 const PostId = () => {
-  const router = useRouter();
+  const { post, user, error, isLoading } = usePost();
+
+  console.log({post, user, error, isLoading });
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Index Page</title>
-      </Head>
       <Header />
-      <div>{router.query.id}</div>
+      <Post />
     </div>
   );
 };
---
+++ src/utils/fetcher.js
@@ -0,0 +1,10 @@
+const fetcher = async (url) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`エラーが発生したため、データの取得に失敗しました`);
+  }
+
+  const json = await response.json();
+  return json;
+};
